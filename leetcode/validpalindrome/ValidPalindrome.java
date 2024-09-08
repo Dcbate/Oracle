d129f04c8c2d770d7286b3b693a44f83f0bf7601
Dcbate <dcbate@hotmail.co.uk>
2024-09-08 10:51:57 +0100

    Update ValidPalindrome.java

--- leetcode/validpalindrome/ValidPalindrome.java
+++ leetcode/validpalindrome/ValidPalindrome.java
@@ -5,29 +5,40 @@ public class ValidPalindrome {
 
     public static boolean isPalindrome(String s){
 
+        s = s.replaceAll(" ","");
+        s = s.replaceAll("[^A-Za-z0-9]", "");
+        s = s.toLowerCase();
+
         char[] array = s.toCharArray();
 
-        String answer= "";
+        boolean even = array.length%2 == 0;
+        int oddLength = (array.length/2);
+        int evenLength = (array.length/2);
 
-
-        for (int i = 0; i<s.length(); i++){
-
-            System.out.println(s.length());
-
-            answer = array[i] + answer;
+        if(even){
+            for(int i =0;i<evenLength;i++){
+                char left =array[i];
+                char right = array[array.length-1-i];
+                if(left != right){
+                    return false;
+                }}
+        }else{
+            for(int i =0;i<oddLength;i++){
+                char left =array[i];
+                char right = array[array.length-1-i];
+                if(left != right){
+                    return false;
+                }}
 
         }
 
-        System.out.println(answer);
-
-
-
         return true;
-    }
+
+    };
 
 
     public static void main(String[] args) {
-        isPalindrome("hello");
+        isPalindrome("Was it a car oor a cat I saw?");
     }
 
 }
